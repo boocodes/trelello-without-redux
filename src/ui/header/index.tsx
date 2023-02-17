34baca5ec9727b22bf598db9c22546e4c4ba3b44
Nick Felker <felker.nick@bk.ru>
2023-02-17 22:55:51 +0600

    init base files

---
+++ src/ui/header/index.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+interface Props{
+
+}
+
+
+function Header(props:Props){
+    return(
+        <ExternalWrapper>
+            Trelello
+        
+        
+        </ExternalWrapper>
+    )
+}
+
+
+const ExternalWrapper = styled.div`
+    background-color: #3d9ee3;
+`
+
+export default Header;
